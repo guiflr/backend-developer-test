4d6be358cf03b3a45029c1f6e75c533970037cfe
guilherme <guiflr97@gmail.com>
2024-02-10 14:59:23 -0300

    chore: remove node_modules

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from 'jest';
+
+const config: Config = {
+  clearMocks: true,
+  coverageProvider: 'v8',
+  testMatch: ['**/__tests__/**/*.spec.[jt]s?(x)'],
+  preset: 'ts-jest',
+  setupFilesAfterEnv: ['<rootDir>/__tests__/singleton.ts'],
+};
+
+export default config;
