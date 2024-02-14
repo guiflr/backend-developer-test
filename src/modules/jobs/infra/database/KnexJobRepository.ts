78d05ccb3b0380a9699ea9812575ff6187b9290a
guilherme <guiflr97@gmail.com>
2024-02-14 14:51:45 -0300

    feat: implement get by id function on KnexJobRepository

--- src/modules/jobs/infra/database/KnexJobRepository.ts
+++ src/modules/jobs/infra/database/KnexJobRepository.ts
@@ -3,10 +3,13 @@ import { JobCreate, JobDTO } from '../../domain/types'
 import { JobRepository } from '../../repositories/JobRepository'
 
 export class KnexJobRepository implements JobRepository {
-  constructor(private knex: Knex){}
-  get(id: string): Promise<JobDTO> {
-    throw new Error('Method not implemented.')
+  constructor (private knex: Knex) {}
+  async get (id: string): Promise<JobDTO> {
+    const jobCreated = await this.knex('jobs').where({ id }).first()
+
+    return jobCreated
   }
+
   async store (job: JobCreate): Promise<JobDTO> {
     const jobCreated = await this.knex('jobs').insert(job).returning(['*'])
 
