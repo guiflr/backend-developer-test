10c99403cc335f93ba2896b2fa28430edbbd0a5b
guilherme <guiflr97@gmail.com>
2024-02-15 23:53:54 -0300

    refactor: add credentials on S3Client

--- src/config/cache/S3Connection.ts
+++ src/config/cache/S3Connection.ts
@@ -2,7 +2,17 @@ import { S3Client } from '@aws-sdk/client-s3'
 
 class S3Connection {
   static connect (): S3Client {
-    return new S3Client({ region: process.env.AWS_REGION })
+    const region = process.env.AWS_REGION as string
+    const accessKeyId = process.env.AWS_ACCESS_KEY_ID as string
+    const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY as string
+
+    return new S3Client({
+      region,
+      credentials: {
+        accessKeyId,
+        secretAccessKey
+      }
+    })
   }
 }
 
