3f05c9e7686192dba67207bc48c9323ebb677721
guilherme <guiflr97@gmail.com>
2024-02-15 22:29:40 -0300

    feat: implementation of aws S3 feed cache

--- src/modules/feed/infra/cache/S3CacheRepository.ts
+++ src/modules/feed/infra/cache/S3CacheRepository.ts
@@ -8,6 +8,25 @@ import { CacheRepository } from '../../repositories/CacheRepository'
 
 export class S3CacheRepository implements CacheRepository {
   constructor (private s3Client: S3Client) {}
+
+  async get (): Promise<FeedData[]> {
+    const command = new GetObjectCommand({
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: process.env.AWS_BUCKET_KEY
+    })
+
+    const data = await this.s3Client.send(command)
+
+    const feedText = await data.Body?.transformToString()
+
+    if (feedText) {
+      const feed = JSON.parse(feedText)
+
+      return feed
+    }
+
+    return []
+  }
   async update (data: FeedData[]): Promise<void> {
     const body = JSON.stringify(data)
     const command = new PutObjectCommand({
